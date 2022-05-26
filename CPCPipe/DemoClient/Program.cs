dd5640445298574da13fe2594a88e5bbb3186b41
FreakLign <32242776+FreakLign@users.noreply.github.com>
2022-05-27 02:29:50 +0800

    u

---
+++ CPCPipe/DemoClient/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+using CPCPipe.Interfaces;
+
+namespace DemoClient
+{
+    class Program
+    {
+        delegate bool ConsoleCtrlDelegate(int dwCtrlType);
+
+        const int CTRL_CLOSE_EVENT = 2;
+
+        [DllImport("Kernel32.dll")]
+        static extern bool SetConsoleCtrlHandler(ConsoleCtrlDelegate handle, bool Add);
+
+        static void Main(string[] args)
+        {
+           
+            if (!File.Exists("DemoServer.exe"))
+            {
+                throw new FileNotFoundException("程序启动失败，服务端程序未找到！");
+            }
+            PipeServer server = new PipeServer();
+            if (server.StartListenning(out var endp))
+            {
+                var addr = $"{endp.Address.ToString()} {endp.Port}";
+                var proc = new Process();
+                proc.StartInfo = new ProcessStartInfo("DemoServer.exe", addr);
+                proc.Start();
+                ConsoleCtrlDelegate ctrlDelegate = new ConsoleCtrlDelegate((e) => {
+                    proc.Kill();
+                    proc.Close();
+                    proc.Dispose();
+                    return true;
+                });
+                SetConsoleCtrlHandler(ctrlDelegate, true);
+            }
+
+            Application application = new Application();
+            application.Run();
+
+        }
+    }
+}
---
+++ CPCPipe/DemoServer/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using System.Windows;
+
+using CPCPipe;
+
+namespace DemoServer
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //if(args == null ||
+            //    args.Length != 1)
+            MessageBox.Show($"Server Application is Running! Para {args[0]} {args[1]}");
+            PipeClient client = new PipeClient();
+            client.ErrMessage += Client_ErrMessage;
+            await client.Connect(new System.Net.IPEndPoint(IPAddress.Parse(args[0]), int.Parse(args[1])));
+
+
+        }
+
+        private static void Client_ErrMessage(string obj)
+        {
+            MessageBox.Show(obj);
+        }
+    }
+}
